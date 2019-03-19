9aebb0c2e3b2f44d9c8aaccb5be1c189b6fa6656
Cedric Champeau <cedric.champeau@gmail.com>
2019-03-19 16:37:52 +0100

    Update to Asciinema 2

--- buildSrc/src/main/java/org/gradle/presentation/asciidoc/ScreencastAsciidoctorExtension.java
+++ buildSrc/src/main/java/org/gradle/presentation/asciidoc/ScreencastAsciidoctorExtension.java
@@ -6,19 +6,12 @@ import org.asciidoctor.extension.InlineMacroProcessor;
 import java.util.Map;
 
 class ScreencastAsciidoctorExtension extends InlineMacroProcessor {
-    private int count = 0;
-
     ScreencastAsciidoctorExtension() {
         super("screencast");
     }
 
     @Override
     protected Object process(AbstractBlock parent, String target, Map<String, Object> attributes) {
-        String pid = "player-container-" + count++;
-        return "\n" +
-                "<div id=\"" + pid + "\"></div>\n" +
-                "  <script>\n" +
-                "    asciinema_player.core.CreatePlayer('" + pid + "', '" + target + ".json', { width: 114, height: 24, fontSize: 'medium' });\n" +
-                "  </script>";
+        return "<asciinema-player src=\"/" + target + ".cast\" class=\"stretch\"></asciinema-player>";
     }
 }
